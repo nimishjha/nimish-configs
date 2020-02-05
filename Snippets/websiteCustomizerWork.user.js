68c7beac4d2cb4474646d2a5fd20790d9c002dad
Nimish Jha <nimish.jha@gmail.com>
2020-02-05 18:08:44 +1100

    update WCW

--- Snippets/websiteCustomizerWork.user.js
+++ Snippets/websiteCustomizerWork.user.js
@@ -10,7 +10,88 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+	'use strict';
+
+	function deleteElementsContainingText(selector, str)
+	{
+		if(!(typeof selector === "string" && selector.length))
+			return;
+		if(!(typeof str === "string" && str.length))
+		{
+			del(selector);
+			return;
+		}
+
+		switch(selector)
+		{
+			case "img": deleteImagesBySrcContaining(str); return;
+			case "a": deleteLinksContainingText(str); return;
+		}
+
+		const e = get(selector);
+		if(e.length)
+		{
+			let i = e.length;
+			while(i--)
+			{
+				const elem = e[i];
+				if(elem.querySelector(selector))
+					continue;
+				if(~elem.textContent.indexOf(str))
+					elem.remove();
+			}
+		}
+		else if(e.parentNode)
+		{
+			if(~e.textContent.indexOf(str))
+				e.remove();
+		}
+	}
+
+	function deleteElementsWithClassContaining(str)
+	{
+		const e = get("*");
+		let i = e.length;
+		while(i--)
+		{
+			const node = e[i];
+			if(~node.className.toString().indexOf(str))
+				del(node);
+		}
+	}
+
+	function deleteLinksContainingText(str)
+	{
+		const links = get("a");
+		let i = links.length;
+		while(i--)
+		{
+			const link = links[i];
+			if(~link.textContent.indexOf(str) || ~link.href.indexOf(str))
+				del(link);
+		}
+	}
+
+	function deleteImagesBySrcContaining(str)
+	{
+		const elems = document.getElementsByTagName("img");
+		let i = elems.length;
+		while(i--)
+		{
+			const elem = elems[i];
+			if(~elem.src.indexOf(str))
+			{
+				xlog("Deleting image with src " + elem.src);
+				elem.remove();
+			}
+		}
+	}
+
+
+	const Nimbus = {
+		logString: "",
+		messageTimeout: null,
+	};
 
 	function get(s)
 	{
@@ -69,6 +150,109 @@
 			style.id = identifier;
 		head.appendChild(style);
 	}
+
+	function showMessage(messageText, msgClass, persist)
+	{
+		clearTimeout(Nimbus.messageTimeout);
+		let messageContainer;
+		msgClass = msgClass || "";
+		const strStyle = 'message { display: block; background: #111; font: 12px Verdcode, Verdana; color: #555; padding: 0 1em; height: 30px; line-height: 30px; position: fixed; bottom: 0; left: 0; width: 100%; z-index: 2000000000; text-align: left; }' +
+		'message.messagebig { font: 32px "Swis721 cn bt"; color: #FFF; height: 60px; line-height: 60px; font-weight: 500; }' +
+		'message.messageerror { color: #F00; background: #500; }';
+
+		if(!get("message"))
+		{
+			messageContainer = createElement("message", { className: msgClass });
+			document.body.insertBefore(messageContainer, document.body.firstChild);
+			if(!getOne("#styleMessage"))
+				insertStyle(strStyle, "styleMessage", true);
+		}
+		else
+		{
+			messageContainer = getOne("message");
+			messageContainer.className = msgClass;
+		}
+		messageContainer.textContent = messageText;
+		if(!persist)
+			Nimbus.messageTimeout = setTimeout(deleteMessage, 2000);
+	}
+
+	function showMessageBig(messageText)
+	{
+		showMessage(messageText, "messagebig");
+	}
+
+	function showMessageError(messageText)
+	{
+		showMessage(messageText, "messagebig messageerror");
+	}
+
+	function deleteMessage()
+	{
+		del("message");
+		del(".xalert");
+		del("#styleMessage");
+	}
+
+	function createElement(tag, props)
+	{
+		const elem = document.createElement(tag);
+		if(props && typeof props === "object")
+		{
+			const keys = Object.keys(props);
+			let i = keys.length;
+			const settableProperties = ["id", "className", "textContent", "innerHTML", "value"];
+			while(i--)
+			{
+				const key = keys[i];
+				if(settableProperties.includes(key))
+					elem[key] = props[key];
+				else
+					elem.setAttribute(key, props[key]);
+			}
+			return elem;
+		}
+		return elem;
+	}
+
+	function replaceElementsBySelector(selector, tagName)
+	{
+		const toReplace = get(selector);
+		if(toReplace.length)
+		{
+			showMessageBig("Replacing " + toReplace.length + " " + selector);
+			let i = toReplace.length;
+			if(tagName === "hr")
+			{
+				while(i--)
+				{
+					const elem = toReplace[i];
+					elem.parentNode.replaceChild(createElement(tagName), elem);
+				}
+			}
+			else
+			{
+				while(i--)
+				{
+					const elem = toReplace[i];
+					const elemId = elem.id;
+					if(elemId)
+						elem.parentNode.replaceChild(createElement(tagName, { id: elemId, innerHTML: elem.innerHTML }), elem);
+					else
+						elem.parentNode.replaceChild(createElement(tagName, { innerHTML: elem.innerHTML }), elem);
+				}
+			}
+		}
+		else if(toReplace && toReplace.parentNode)
+		{
+			showMessageBig("Replacing one " + selector);
+			toReplace.parentNode.replaceChild(createElement(tagName, { innerHTML: toReplace.innerHTML }), toReplace);
+		}
+	}
+
+	//
+	//
+	//
 
 	function doSlack()
 	{
@@ -138,6 +322,21 @@
 		insertStyle(s, "styleYoutube", true);
 	}
 
+	function redirectReddit()
+	{
+		if(~location.href.indexOf("www.reddit.com"))
+			location.href = location.href.replace(/www\./, "old.");
+	}
+
+	function cleanupReddit()
+	{
+		if(~location.href.indexOf("/comments/"))
+		{
+			replaceElementsBySelector(".thing", "comment");
+			deleteElementsContainingText("ul", "permalink");
+		}
+	}
+
 	function doBupaDevEnvironments()
 	{
 		if(get(".cover-detail"))
@@ -181,6 +380,12 @@
 			case 'youtube.com':
 			case 'www.youtube.com':
 				doYoutube();
+				break;
+			case 'www.reddit.com':
+				redirectReddit();
+				break;
+			case 'old.reddit.com':
+				cleanupReddit();
 				break;
 			case 'mywork.bupa.com.au':
 				setTimeout(doBupaMyWork, 1000);
