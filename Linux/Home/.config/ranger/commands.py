c48fc1d04a52aa31600446fed057334fd6a9f1a8
Nimish Jha <nimish.jha@gmail.com>
2024-07-03 18:08:44 +1000

    ranger custom commands

--- Linux/Home/.config/ranger/commands.py
+++ Linux/Home/.config/ranger/commands.py
@@ -1,62 +1,65 @@
-# This is a sample commands.py.  You can add your own commands here.
-#
-# Please refer to commands_full.py for all the default commands and a complete
-# documentation.  Do NOT add them all here, or you may end up with defunct
-# commands when upgrading ranger.
-
-# A simple command for demonstration purposes follows.
-# -----------------------------------------------------------------------------
-
-from __future__ import (absolute_import, division, print_function)
-
-# You can import any python module as needed.
 import os
 
-# You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import Command
 
+class prepend_aaa(Command):
 
-# Any class that is a subclass of "Command" will be integrated into ranger as a
-# command.  Try typing ":my_edit<ENTER>" in ranger!
-class my_edit(Command):
-    # The so-called doc-string of the class will be visible in the built-in
-    # help that is accessible by typing "?c" inside ranger.
-    """:my_edit <filename>
+	def execute(self):
+		from ranger.container.file import File
+		from os import access
+		from os.path import splitext
 
-    A sample command for demonstration purposes that opens a file in an editor.
-    """
+		for index, file in enumerate( self.fm.thistab.get_selection() ):
+			new_name = "aaa " + file.relative_path
+			if access(new_name, os.F_OK):
+				return self.fm.notify("Batch rename failed, file already exists", bad=True)
+			try:
+				os.rename(file.relative_path, new_name)
+			except OSError as err:
+				self.fm.notify(err)
+				return False
 
-    # The execute method is called when you run this command in ranger.
-    def execute(self):
-        # self.arg(1) is the first (space-separated) argument to the function.
-        # This way you can write ":my_edit somefilename<ENTER>".
-        if self.arg(1):
-            # self.rest(1) contains self.arg(1) and everything that follows
-            target_filename = self.rest(1)
-        else:
-            # self.fm is a ranger.core.filemanager.FileManager object and gives
-            # you access to internals of ranger.
-            # self.fm.thisfile is a ranger.container.file.File object and is a
-            # reference to the currently selected file.
-            target_filename = self.fm.thisfile.path
+		return None
 
-        # This is a generic function to print text in ranger.
-        self.fm.notify("Let's edit the file " + target_filename + "!")
+class batch_rename(Command):
 
-        # Using bad=True in fm.notify allows you to print error messages:
-        if not os.path.exists(target_filename):
-            self.fm.notify("The given file does not exist!", bad=True)
-            return
+	def execute(self):
+		from ranger.container.file import File
+		from os import access
+		from os.path import splitext
 
-        # This executes a function from ranger.core.acitons, a module with a
-        # variety of subroutines that can help you construct commands.
-        # Check out the source, or run "pydoc ranger.core.actions" for a list.
-        self.fm.edit_file(target_filename)
+		new_base_name = self.rest(1)
 
-    # The tab method is called when you press tab, and should return a list of
-    # suggestions that the user will tab through.
-    # tabnum is 1 for <TAB> and -1 for <S-TAB> by default
-    def tab(self, tabnum):
-        # This is a generic tab-completion function that iterates through the
-        # content of the current directory.
-        return self._tab_directory_content()
+		for index, file in enumerate( self.fm.thistab.get_selection() ):
+			new_name = new_base_name + "%04d" % (index) + splitext(file.relative_path)[1]
+			if access(new_name, os.F_OK):
+				return self.fm.notify("Batch rename failed, file already exists", bad=True)
+			try:
+				os.rename(file.relative_path, new_name)
+			except OSError as err:
+				self.fm.notify(err)
+				return False
+
+		self.fm.notify("Batch rename successful")
+
+		return None
+
+class remove_underscores(Command):
+
+	def execute(self):
+		from ranger.container.file import File
+		from os import access
+		from os.path import splitext
+
+		for index, file in enumerate( self.fm.thistab.get_selection() ):
+			new_name = file.relative_path.replace("_", " ")
+			if access(new_name, os.F_OK):
+				return self.fm.notify("Batch rename failed, file already exists", bad=True)
+			try:
+				os.rename(file.relative_path, new_name)
+			except OSError as err:
+				self.fm.notify(err)
+				return False
+
+		return None
+
