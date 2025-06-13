1a314ae0a4f4ead40789ab3764f318765b488b58
Nimish Jha <nimish.jha@gmail.com>
2025-06-13 13:27:41 +1000

    Firefox

---
+++ Firefox/user.js
@@ -0,0 +1,46 @@
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.aboutwelcome.didSeeFinalScreen", true);
+user_pref("browser.cache.disk.enable", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("extensions.pocket.enabled", false);
+user_pref("ui.textHighlightBackground", "#CC00CC");
+user_pref("ui.textHighlightForeground", "#00CC00");
+user_pref("ui.textSelectAttentionBackground", "#CC6600");
+user_pref("ui.textSelectAttentionForeground", "#000000");
+user_pref("ui.textSelectDisabledBackground", "#804000");
+user_pref("sidebar.visibility", "hide-sidebar");
+user_pref("privacy.clearHistory.formdata", true);
+user_pref("privacy.clearOnShutdown_v2.formdata", true);
+user_pref("privacy.fingerprintingProtection", true);
+user_pref("browser.contentblocking.category", "strict");
+user_pref("pdfjs.forcePageColors", true);
+user_pref("pdfjs.pageColorsBackground", "#000000");
+user_pref("pdfjs.pageColorsForeground", "#777777");
+user_pref("browser.helperApps.showOpenOptionForPdfJS", false);
+user_pref("browser.pdf.launchDefaultEdgeAsApp", false);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("browser.urlbar.suggest.engines", false);
+user_pref("browser.urlbar.suggest.history", false);
+user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("browser.urlbar.suggest.recentsearches", false);
+user_pref("browser.urlbar.suggest.searches", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("devtools.theme", "dark");
+user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
+user_pref("browser.urlbar.placeholderName.private", "DuckDuckGo");
+user_pref("browser.download.alwaysOpenPanel", false);
+user_pref("browser.download.panel.shown", true);
+user_pref("browser.preferences.experimental.hidden", false);
+
+
+user_pref("browser.sessionstore.closedTabsFromAllWindows", false);
+user_pref("browser.sessionstore.closedTabsFromClosedWindows", false);
+user_pref("browser.sessionstore.disable_platform_collection", true);
+user_pref("browser.sessionstore.interval", 15000000);
+user_pref("browser.sessionstore.max_tabs_undo", 1);
+user_pref("browser.sessionstore.max_windows_undo", 1);
+user_pref("browser.sessionstore.resume_from_crash", false);
+
+
