9fd774717013c406ae1cde30f97e88e4528e376a
Nimish Jha <nimish.jha@gmail.com>
2025-07-01 19:40:57 +1000

    micro/colorSchemeSwitcher: auto-load theme files

--- Linux/Home/.config/micro/plug/colorSchemeSwitcher/colorSchemeSwitcher.lua
+++ Linux/Home/.config/micro/plug/colorSchemeSwitcher/colorSchemeSwitcher.lua
@@ -3,94 +3,19 @@ VERSION = "0.0.1"
 local micro = import("micro")
 local config = import("micro/config")
 
-local colorSchemes = {
-	"gen00",
-	"gen01",
-	"gen02",
-	"gen03",
-	"gen04",
-	"gen05",
-	"gen06",
-	"gen07",
-	"gen08",
-	"gen09",
-	"gen10",
-	"gen11",
-	"gen12",
-	"gen13",
-	"gen14",
-	"gen15",
-	"gen16",
-	"gen17",
-	"gen18",
-	"gen19",
-	"gen20",
-	"gen21",
-	"gen22",
-	"gen23",
-	"gen24",
-	"gen25",
-	"gen26",
-	"gen27",
-	"gen28",
-	"gen29",
-	"gen30",
-	"gen31",
-	"gen32",
-	"gen33",
-	"gen34",
-	"gen35",
-	"gen36",
-	"gen37",
-	"gen38",
-	"gen39",
-	"gen40",
-	"gen41",
-	"gen42",
-	"gen43",
-	"gen44",
-	"gen45",
-	"gen46",
-	"gen47",
-	"gen48",
-	"gen49",
-	"gen50",
-	"gen51",
-	"gen52",
-	"gen53",
-	"gen54",
-	"gen55",
-	"gen56",
-	"gen57",
-	"gen58",
-	"gen59",
-	"gen60",
-	"gen61",
-	"gen62",
-	"gen63",
-	"gen64",
-	"gen65",
-	"gen66",
-	"gen67",
-	"gen68",
-	"gen69",
-	"gen70",
-	"gen71",
-	"gen72",
-	"gen73",
-	"gen74",
-	"gen75",
-	"gen76",
-	"gen77",
-	"gen78",
-	"gen79",
-	"gen80",
-	"gen81",
-	"gen82",
-	"gen83",
-	"gen84",
-}
+local DIR = "~/.config/micro/colorschemes"
 
+function getThemeFiles(dir)
+	local f = io.popen("ls -1p " .. dir, "r")
+	local dir = {}
+	for entry in f:lines() do
+		dir[#dir + 1] = entry:gsub("\.micro", "")
+	end
+
+	return dir
+end
+
+local colorSchemes = getThemeFiles(DIR)
 
 function getNextString(array, currentString)
 	local currentIndex = nil
@@ -150,6 +75,7 @@ function firstColorScheme()
 end
 
 function init()
+	-- micro.InfoBar():Message("colorSchemeSwitcher: " .. #colorSchemes .. " themes")
 	config.MakeCommand("firstcolorscheme", firstColorScheme, config.NoComplete)
 	config.MakeCommand("nextcolorscheme", nextColorScheme, config.NoComplete)
 	config.MakeCommand("previouscolorscheme", previousColorScheme, config.NoComplete)
