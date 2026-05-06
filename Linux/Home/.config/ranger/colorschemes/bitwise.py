89ee72c81c3379f9ba900cc7d0a5e3c4f7b4004c
Nimish Jha <nimish.jha@gmail.com>
2026-05-06 10:50:43 +1000

    ranger bitwise color scheme: put COLORS_BY_EXTENSION where it belongs

--- Linux/Home/.config/ranger/colorschemes/bitwise.py
+++ Linux/Home/.config/ranger/colorschemes/bitwise.py
@@ -3,13 +3,103 @@ from ranger.gui.color import *
 
 import ranger.customizations.extensionsasbitmask as extasbitmask
 
+COLORS_BY_EXTENSION = {
+	'NOEXT':        240,
+
+	'jpg':          100,
+	'jpeg':         101,
+	'png':          102,
+	'webp':         103,
+	'gif':          104,
+	'bmp':          105,
+	'svg':          106,
+
+	'mkv':          173,
+	'webm':         176,
+	'mp4':          175,
+	'm2ts':         173,
+	'wmv':          174,
+	'avi':          175,
+	'mov':          176,
+	'mpg':          177,
+	'm4v':          178,
+	'flv':          179,
+	'3gp':          179,
+
+	'flac':         120,
+	'ogg':          121,
+	'ape':          122,
+	'opus':         123,
+	'mp3':          124,
+	'aac':          125,
+	'wav':          126,
+	'm4a':          127,
+
+	'epub':         130,
+	'mobi':         131,
+	'azw':          132,
+	'azw3':         133,
+	'htmlz':        134,
+
+	'rar':          140,
+	'zip':          141,
+	'7z':           142,
+	'gz':           143,
+	'xz':           144,
+	'bz':           145,
+	'bz2':          146,
+
+	'c':            150,
+	'cpp':          151,
+	'lua':          152,
+	'js':           153,
+	'glsl':         153,
+	'hlsl':         152,
+	'php':          153,
+	'py':           150,
+	'sh':           155,
+	'go':           150,
+	'rb':           151,
+	'odin':         152,
+
+	'rs':           160,
+	'fx':           161,
+	'zig':          162,
+	'mjs':          163,
+	'ts':           164,
+	'tsx':          164,
+
+	'bash_profile': 170,
+	'bashrc':       171,
+	'css':          172,
+	'json':         173,
+	'conf':         174,
+	'yaml':         175,
+	'toml':         176,
+	'h':            177,
+	'hpp':          178,
+	'gitignore':    179,
+
+	'log':          180,
+	'txt':          181,
+	'md':           182,
+	'csv':          183,
+
+	'html':         195,
+	'htm':          196,
+	'xml':          197,
+
+	'pdf':          207,
+	'djvu':         205,
+}
+
 NO_COLOR_FOUND = 239
 
 def getFgByExtension(context):
-	ext = extasbitmask.getExtensionFromBits([context.exbit0, context.exbit1, context.exbit2, context.exbit3, context.exbit4, context.exbit5, context.exbit6, context.exbit7 ])
-	return extasbitmask.COLORS_BY_EXTENSION.get(ext) or NO_COLOR_FOUND
+	ext = extasbitmask.getExtensionFromBits([context.exbit0, context.exbit1, context.exbit2, context.exbit3, context.exbit4, context.exbit5, context.exbit6, context.exbit7])
+	return COLORS_BY_EXTENSION.get(ext) or NO_COLOR_FOUND
 
-class DarkGray(ColorScheme):
+class Bitwise(ColorScheme):
 	progress_bar_color = 240
 
 	def use(self, context):
@@ -24,14 +114,6 @@ class DarkGray(ColorScheme):
 		elif context.in_browser:
 			if context.border:
 				fg = 240
-			if context.audio:
-				fg = 136
-			if context.video:
-				fg = 99
-			if context.image:
-				fg = 33
-			if context.document:
-				fg = 30
 
 			if context.exbit0 is not None:
 				fg = getFgByExtension(context)
@@ -40,7 +122,7 @@ class DarkGray(ColorScheme):
 				attr |= bold
 				fg = 120
 			if context.marked:
-				bg = 17
+				bg = 234
 			if context.directory:
 				attr |= bold
 				fg = 246
@@ -57,12 +139,12 @@ class DarkGray(ColorScheme):
 			if context.cut:
 				bg = 52
 			if context.copied:
-				bg = 55
-				fg = 252
+				bg = 17
+				fg = 15
 			if context.marked and context.selected:
-				bg = 20
+				bg = 233
 			if context.copied and context.selected:
-				bg = 57
+				bg = 233
 			if context.cut and context.selected:
 				bg = 88
 			if not(context.main_column):
@@ -104,16 +186,13 @@ class DarkGray(ColorScheme):
 			if context.marked:
 				attr |= bold | reverse
 				fg = yellow
-				fg += BRIGHT
 			if context.frozen:
 				attr |= bold | reverse
 				fg = cyan
-				fg += BRIGHT
 			if context.message:
 				if context.bad:
 					attr |= bold
 					fg = red
-					fg += BRIGHT
 			if context.loaded:
 				bg = self.progress_bar_color
 			if context.vcsinfo:
